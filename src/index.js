ad59b8441ff5167380d7c3633b85fe6600a6a087
atul singh <atul.singh@myglamm.com>
2020-03-19 12:40:37 +0530

    changed courses structure

--- src/index.js
+++ src/index.js
@@ -2,29 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7
-    },
-    {
-      name: "State of a component",
-      exercises: 14
-    }
-  ];
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7
+          },
+          {
+            name: 'State of a component',
+            exercises: 14
+          }
+        ]
+      }
 
   const Header = props => {
     console.log(props);
-    return <h1>{props.course}</h1>;
+    return <h1>{props.course.name}</h1>;
   };
 
   const Content = props => {
-    return props.parts.map(item => {
+    return props.course.parts.map(item => {
       return (
         <p>
           {item.name}: {item.exercises}
@@ -35,7 +37,7 @@ const App = () => {
 
   const Total = props => {
     let counter = 0;
-    props.parts.map(item => {
+    props.course.parts.map(item => {
       counter += item.exercises;
     });
     return `Total exercises: ${counter}`;
@@ -44,8 +46,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
